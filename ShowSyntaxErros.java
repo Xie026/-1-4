a932668dabfdc7c63320ee8bd3033489e2b80936
Xie026 <71316959+Xie026@users.noreply.github.com>
2020-09-22 15:53:42 +0800

    Create ShowSyntaxErros.java

---
+++ ShowSyntaxErros.java
@@ -0,0 +1,11 @@
+
+public class ShowSyntaxErrors {
+
+	public static main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to java);
+	}
+
+}
+错误：1、第2行main方法前遗漏关键字void
+      2、第3行的字符串Welcome to java应该加上引号
